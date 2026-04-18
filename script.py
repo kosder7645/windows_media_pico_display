eb43d8d1995642c68f9133cb96e804476034e1e4
kosder7645 <kostrowka123@gmail.com>
2026-04-18 05:47:13 +0200

    dupa dupa

--- script.py
+++ script.py
@@ -1,10 +1,25 @@
+import asyncio
 import serial
-import time
+from winsdk.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
 
-ser = serial.Serial('COM6', 115200)  # użyj portu Pico
-time.sleep(2)
+ser = serial.Serial('COM6', 9600)  # zamień na swój port
 
-while True:
-    text = input("Wpisz tekst do wysłania: ")
-    ser.write((text + "\n").encode('utf-8'))
-    print("Wysłano:", text)
+async def send_now_playing():
+    manager = await MediaManager.request_async()
+    session = manager.get_current_session()
+    if not session:
+        return
+    info = await session.try_get_media_properties_async()
+    title = info.title
+    artist = info.artist
+    data = f"{title} - {artist}\n"
+    print(title)
+    print(artist)
+    ser.write(data.encode())
+
+async def main():
+    while True:
+        await send_now_playing()
+        await asyncio.sleep(2)
+
+asyncio.run(main())
